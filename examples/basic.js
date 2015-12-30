6a6f1711430e8ed79a29cafa09eca27d31554c69
Keith Beckman <keith.beckman@mobilelabsinc.com>
2015-12-29 21:08:33 -0500

    sanitize paths; better title handling; better chapter id derivation

--- examples/basic.js
+++ examples/basic.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 var epubGenerator = require('../index.js');
 
-var xhtml = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>HELLO</title></head><body>Hello world from epub-generator!</body></html>';
+var xhtml = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>HELLO</title></head><body><div>Hello world from epub-generator!</div></body></html>';
 
 var epubStream = epubGenerator({
 		title: 'Hello World',
